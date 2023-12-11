48bcf2a47e00ebbd0fe974a7d6e6405be5f0520d
takemorebreaks <sivaprasadzag@gmail.com>
2023-12-11 23:10:07 +0530

    It is the beginning of project ,YesManthra is a online shopping website

---
+++ src/main/java/com/example/YesManthra/YesManthraApplication.java
@@ -0,0 +1,13 @@
+package com.example.YesManthra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesManthraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesManthraApplication.class, args);
+	}
+
+}
